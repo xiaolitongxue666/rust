5d53d26a3e98b0d683c88b36de3dbfecf9763429
xiaolitongxue666 <leonpa1987@gmail.com>
2022-01-17 15:18:41 +0800

    .

--- lucians-luscious-lasagna/src/lib.rs
+++ lucians-luscious-lasagna/src/lib.rs
@@ -8,23 +8,26 @@ pub fn expected_minutes_in_oven() -> i32 {
 }
 
 pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate remaining minutes in oven given actual minutes in oven: {}",
-        actual_minutes_in_oven
-    )
+    // unimplemented!(
+    //     "calculate remaining minutes in oven given actual minutes in oven: {}",
+    //     actual_minutes_in_oven
+    // )
+    return 40 - actual_minutes_in_oven
 }
 
 pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
-    unimplemented!(
-        "calculate preparation time in minutes for number of layers: {}",
-        number_of_layers
-    )
+    // unimplemented!(
+    //     "calculate preparation time in minutes for number of layers: {}",
+    //     number_of_layers
+    // )
+    return number_of_layers * 2
 }
 
 pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    unimplemented!(
-        "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
-        number_of_layers,
-        actual_minutes_in_oven
-    )
+    // unimplemented!(
+    //     "calculate elapsed time in minutes for number of layers {} and actual minutes in oven {}",
+    //     number_of_layers,
+    //     actual_minutes_in_oven
+    // )
+    return number_of_layers*2+actual_minutes_in_oven
 }
