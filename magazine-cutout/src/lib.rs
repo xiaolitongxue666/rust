da039c4300b5bbd9cb062c3bb4300dc53ea66c3d
xiaolitongxue666 <leonpa1987@gmail.com>
2022-01-19 17:24:25 +0800

    .

---
+++ magazine-cutout/src/lib.rs
@@ -0,0 +1,12 @@
+// This stub file contains items which aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+#![allow(unused)]
+
+use std::collections::HashMap;
+
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
+    // unimplemented!()
+    let mut magazine_words = HashMap::new();
+
+    magazine
+}
