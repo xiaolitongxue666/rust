006d4fb3c020697482536ea1b52ae2069d06c0d3
liyong <leonpa1987@gmail.com>
2022-04-29 22:48:55 +0800

    space age each own trite for every plant work well

--- space-age/tests/space-age.rs
+++ space-age/tests/space-age.rs
@@ -5,7 +5,7 @@ fn assert_in_delta(expected: f64, actual: f64) {
     let delta: f64 = 0.01;
     if diff > delta {
         panic!(
-            "Your result of {} should be within {} of the expected result {}",
+            "\nYour result of {} should be within {} of the expected result {}",
             actual, delta, expected
         )
     }
@@ -18,49 +18,49 @@ fn earth_age() {
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn mercury_age() {
     let duration = Duration::from(2_134_835_688);
     assert_in_delta(280.88, Mercury::years_during(&duration));
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn venus_age() {
     let duration = Duration::from(189_839_836);
     assert_in_delta(9.78, Venus::years_during(&duration));
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn mars_age() {
     let duration = Duration::from(2_129_871_239);
     assert_in_delta(35.88, Mars::years_during(&duration));
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn jupiter_age() {
     let duration = Duration::from(901_876_382);
     assert_in_delta(2.41, Jupiter::years_during(&duration));
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn saturn_age() {
     let duration = Duration::from(2_000_000_000);
     assert_in_delta(2.15, Saturn::years_during(&duration));
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn uranus_age() {
     let duration = Duration::from(1_210_123_456);
     assert_in_delta(0.46, Uranus::years_during(&duration));
 }
 
 #[test]
-#[ignore]
+// #[ignore]
 fn neptune_age() {
     let duration = Duration::from(1_821_023_456);
     assert_in_delta(0.35, Neptune::years_during(&duration));
